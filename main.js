53f8bd491840d85a665727360c841cc5370d84da
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-04 11:00:34 +0800

    База подготовлена, осталось допилить задачу

---
+++ main.js
@@ -0,0 +1,10 @@
+function onRowClick(row) {
+  let title = document.getElementById("title")
+  title.innerText = row.dataset.title;
+
+  // находим все теги tr, которые внутри tbody, который внутри тега с классом table
+  document.querySelectorAll(".table tbody tr").forEach(el => {
+    el.classList.remove("table-info"); // удаляем у них класс table-info
+  })
+  row.classList.add("table-info");
+}
