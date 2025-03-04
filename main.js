98b84d902f5ea266748b5d8e570d2cc40b8f8aef
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-04 11:24:01 +0800

    Задание выполнено

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ function onRowClick(row) {
   let image = document.getElementById("image")
   image.src = row.dataset.image;
 
+  let director = document.getElementById("director")
+  director.innerText = row.dataset.director;
+  let year = document.getElementById("year")
+  year.innerText = row.dataset.year;
+  let rating = document.getElementById("rating")
+  rating.innerText = row.dataset.rating;
+
   // находим все теги tr, которые внутри tbody, который внутри тега с классом table
   document.querySelectorAll(".table tbody tr").forEach(el => {
     el.classList.remove("table-info"); // удаляем у них класс table-info
